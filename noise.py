fd0eb2e8bbf4a4682b13509de92d56f2d499ed52
Caleb Ginorio <caleb.ginorio@gmail.com>
2023-02-02 22:14:36 -0500

    Update noise.py

--- noise.py
+++ noise.py
@@ -134,7 +134,7 @@ if selected_equation == "Noise Dose":
     def dose(c, t):
         return 100 * sum(ci / ti for ci, ti in zip(c, t))
     st.write("### Dose Calculation")
-    n = st.int_input("How many C and T values do you want to enter?")
+    n = int(st.number_input("How many C and T values do you want to enter?", step =1))
     c = [st.text_input(f"Enter C{i}") for i in range(1, n + 1)]
     t = [st.text_input(f"Enter T{i}") for i in range(1, n + 1)]
     if all(c) and all(t):
